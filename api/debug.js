9ff5639a9730df959bb4496f21b272fccf9408a1
dispanart <86879580+dispanart@users.noreply.github.com>
2026-03-24 15:57:59 +0700

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.GEMINI_API_KEY;
-  res.status(200).json({
-    keyExists: !!key,
-    keyLength: key ? key.length : 0,
-    keyStart: key ? key.substring(0, 8) + '...' : 'TIDAK ADA',
-  });
-}
